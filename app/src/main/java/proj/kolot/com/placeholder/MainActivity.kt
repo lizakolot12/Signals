33c30336350ea2209dbe84b4ccc6778f914800a5
Liza Kolot <olgaficusear@gmail.com>
2019-10-21 12:25:47 +0300

    add navigation drawer

--- app/src/main/java/proj/kolot/com/placeholder/MainActivity.kt
+++ app/src/main/java/proj/kolot/com/placeholder/MainActivity.kt
@@ -2,22 +2,38 @@ package proj.kolot.com.placeholder
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import com.facebook.*
 import com.facebook.login.LoginResult
 import com.facebook.login.widget.LoginButton
+import com.google.android.material.navigation.NavigationView
 import org.json.JSONException
+
+
 
 
 
 
 class MainActivity : AppCompatActivity() {
     private var callbackManager: CallbackManager? = null
+    private lateinit var toogle:ActionBarDrawerToggle
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         callbackManager = CallbackManager.Factory.create()
+        var test = findViewById<TextView>(R.id.textView)
+        test.setOnClickListener(View.OnClickListener {
+          //  val intent = Intent(this, GeneralActivity::class.java)
+          //  startActivity(intent)
+        })
         val loginButton = findViewById(R.id.login_button) as LoginButton
         loginButton.registerCallback(callbackManager, object : FacebookCallback<LoginResult> {
             override fun onSuccess(loginResult: LoginResult) {
@@ -33,7 +49,43 @@ class MainActivity : AppCompatActivity() {
                 // App code
             }
         })
+
+        val dl: DrawerLayout =   findViewById(R.id.activity_main) as DrawerLayout
+        toogle = ActionBarDrawerToggle(this, dl, R.string.Open, R.string.Close)
+
+        dl.addDrawerListener(toogle)
+        toogle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val nv = findViewById(R.id.navigationView) as NavigationView
+        nv.setNavigationItemSelectedListener(object :
+            NavigationView.OnNavigationItemSelectedListener {
+             override fun onNavigationItemSelected(item: MenuItem): Boolean {
+                val id = item.getItemId()
+                when (id) {
+                    R.id.account -> Toast.makeText(
+                        this@MainActivity,
+                        "My Account",
+                        Toast.LENGTH_SHORT
+                    ).show()
+
+                    else -> return true
+                }
+
+
+                return true
+
+            }
+        })
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        return if (toogle.onOptionsItemSelected(item)) true else super.onOptionsItemSelected(item)
+
+    }
+
     public override fun onStart() {
         super.onStart()
         val accessToken = AccessToken.getCurrentAccessToken()
@@ -41,6 +93,7 @@ class MainActivity : AppCompatActivity() {
             useLoginInformation(accessToken)
         }
     }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         callbackManager?.onActivityResult(requestCode, resultCode, data)
         super.onActivityResult(requestCode, resultCode, data)
